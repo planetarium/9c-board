6cfe20130f607f5b6e8e2a2e0e71994d806cb0ef
moreal <dev.moreal@gmail.com>
2020-04-12 01:50:54 +0900

    feat(#1): implement ranking page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
+interface RankingRowProps {
+  data: [string, RankingInfo]
+}
+
+type Address = string
+
+interface RankingInfo {
+  exp: string;
+  updatedAt: string;
+  level: string;
+  avatarAddress: string;
+  stageClearedBlockIndex: string;
+  armorId: string;
+  avatarName: string;
+  agentAddress: string;
+}
+
+interface AddressRankingInfoMap {
+  [address: string]: RankingInfo
+}
+
+interface RankingState {
+  map: AddressRankingInfoMap
+  address: Address
+}
+
+interface RankingStateResponse {
+  value: RankingState
+}
+
+const RankingRow: React.FC<RankingRowProps> = (props) => (
+  <div>{props.data[1].avatarName.split(' ')[0]}#{props.data[0].substring(0, 6)} - exp={props.data[1].exp}</div>
+);
+
+const HOST = "http://a4af4ffa3787011ea824802399f8ed0e-2108883843.ap-northeast-2.elb.amazonaws.com";
+
+const App: React.FC = () => {
+  const [value, setValue] = React.useState<RankingState>();
+
+  const getStateUrl = (address: string) => `${HOST}/state/${address}`;
+
+  const loadRankingState = () => {
+    const RANKING_STATE_URL = getStateUrl("0000000000000000000000000000000000000001");
+    axios.get<RankingStateResponse>(RANKING_STATE_URL).then(({ data: { value } }) => {
+      setValue(value);
+    });
+  }
+
+  useEffect(() => loadRankingState());
+  
+  const rankingRows = value === undefined
+    ? (<>Loading</>)
+    : Object.entries(value?.map || []).map((xs: [string, RankingInfo]) => (<RankingRow data={xs}/>));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Ranking</h1>
+      {rankingRows}
+    </>
   );
 }
 
