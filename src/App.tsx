1a344a6b32d25c0f71fa186a84ab88cb73eb4766
moreal <dev.moreal@gmail.com>
2020-04-19 08:43:54 +0900

    chore: add link bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { RankPage, StatePage } from "./pages";
 import './App.css';
 
 const App: React.FC = () => (
   <Router>
+    <div style={{height: "30px", padding: "5px 0", borderBottom: "solid black 1px"}}>
+      <Link to="/rank" style={{padding: "0 10px", fontSize: "20px", width: "20px", textAlign: "center", display: "block", textDecoration: "none", color: "black"}}>Rank</Link>
+    </div>
     <Switch>
       <Route path="/rank">
         <RankPage />
