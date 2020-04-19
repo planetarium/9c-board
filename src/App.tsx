9ec17b1c79b65eefd68055558468b97596db5141
moreal <dev.moreal@gmail.com>
2020-04-20 04:43:38 +0900

    refactor: replace inline style wiwith styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { RankPage, StatePage } from "./pages";
 import './App.css';
+import styled from "styled-components";
+
+const TopMenuBar = styled.div`
+  height: 30px;
+  padding: 5px 0;
+  border-bottom: solid black 1px;
+`;
+
+const TopMenuLink = styled(Link)`
+  padding: 0 10px;
+  font-size: 20px;
+  width: 20px;
+  text-align: center;
+  display: block;
+  text-decoration: none;
+  color: black;
+`;
 
 const App: React.FC = () => (
   <Router>
-    <div style={{height: "30px", padding: "5px 0", borderBottom: "solid black 1px"}}>
-      <Link to="/rank" style={{padding: "0 10px", fontSize: "20px", width: "20px", textAlign: "center", display: "block", textDecoration: "none", color: "black"}}>Rank</Link>
-    </div>
+    <TopMenuBar>
+      <TopMenuLink to="/rank">Rank</TopMenuLink>
+    </TopMenuBar>
     <Switch>
       <Route path="/rank">
         <RankPage />
