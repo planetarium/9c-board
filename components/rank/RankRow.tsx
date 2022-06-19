29ac3c1a4b5016a88f6d92f1a480f9d27bd776bf
Lee Dogeon <dev.moreal@gmail.com>
2022-06-19 20:51:00 +0900

    feat: link to avatar page

--- components/rank/RankRow.tsx
+++ components/rank/RankRow.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 export interface RankRowProps {
     rank: number,
@@ -8,8 +9,10 @@ export interface RankRowProps {
 
 export const RankRow: NextPage<RankRowProps> = ({ rank, name, avatarAddress }) => {
     return (
-        <div className="w-full md:w-auto m-3 mx-12 p-4 border-solid border-2 border-gray-700">
-            {rank} {name} {avatarAddress}
-        </div>
+        <Link href={`/avatar/${avatarAddress}`}>
+            <div className="w-full md:w-auto m-3 mx-12 p-4 border-solid border-2 border-gray-700">
+                {rank} {name} {avatarAddress}
+            </div>
+        </Link>
     )
 }
