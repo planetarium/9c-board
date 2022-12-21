5b8cd49a881e47d5f169d3ca5764bb7887b7898e
moreal <dev.moreal@gmail.com>
2022-12-21 10:21:25 +0900

    Rename `GRAPHQL_ENDPOINT` enviornment variable to `MAINNET_GRAPHQL_ENDPOINT`

--- sdk.ts
+++ sdk.ts
@@ -1,11 +1,11 @@
 import { getSdk } from "./generated/graphql-request";
 import { GraphQLClient } from "graphql-request"
 
-if (process.env.GRAPHQL_ENDPOINT === undefined) {
+if (process.env.MAINNET_GRAPHQL_ENDPOINT === undefined) {
     throw new Error("All required environment variables are not set.");
 }
 
-const client = new GraphQLClient(process.env.GRAPHQL_ENDPOINT);
+const client = new GraphQLClient(process.env.MAINNET_GRAPHQL_ENDPOINT);
 const headlessGraphQLSDK = getSdk(client);
 
 export default headlessGraphQLSDK;
