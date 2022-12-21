3507d8fd2fd65f36eea3bea6d9a00370e903168b
moreal <dev.moreal@gmail.com>
2022-12-21 10:24:36 +0900

    Support internal GraphQL client

--- sdk.ts
+++ sdk.ts
@@ -1,11 +1,14 @@
 import { getSdk } from "./generated/graphql-request";
 import { GraphQLClient } from "graphql-request"
 
-if (process.env.MAINNET_GRAPHQL_ENDPOINT === undefined) {
+if (process.env.MAINNET_GRAPHQL_ENDPOINT === undefined || process.env.INTERNAL_GRAPHQL_ENDPOINT === undefined) {
     throw new Error("All required environment variables are not set.");
 }
 
 const client = new GraphQLClient(process.env.MAINNET_GRAPHQL_ENDPOINT);
 const headlessGraphQLSDK = getSdk(client);
+
+const internalNetworkClient = new GraphQLClient(process.env.INTERNAL_GRAPHQL_ENDPOINT);
+export const internalGraphQLSDK = getSdk(internalNetworkClient);
 
 export default headlessGraphQLSDK;
