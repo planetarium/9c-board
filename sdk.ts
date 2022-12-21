93af668f6613fcaba5c6c67df57af67b56e12be0
moreal <dev.moreal@gmail.com>
2022-12-21 09:50:48 +0900

    Remove Hasura requirements

--- sdk.ts
+++ sdk.ts
@@ -1,12 +1,11 @@
 import { getSdk } from "./generated/graphql-request";
 import { GraphQLClient } from "graphql-request"
 
-if (process.env.GRAPHQL_ENDPOINT === undefined || process.env.HASURA_ADMIN_SECRET === undefined) {
+if (process.env.GRAPHQL_ENDPOINT === undefined) {
     throw new Error("All required environment variables are not set.");
 }
 
 const client = new GraphQLClient(process.env.GRAPHQL_ENDPOINT);
-client.setHeader("x-hasura-admin-secret", process.env.HASURA_ADMIN_SECRET);
 const headlessGraphQLSDK = getSdk(client);
 
 export default headlessGraphQLSDK;
