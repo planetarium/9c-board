65f19333664cc537dfacb31ddac527fe5dedfb1f
moreal <dev.moreal@gmail.com>
2024-04-04 10:22:17 +0900

    Disable node:fs fallback

--- next.config.js
+++ next.config.js
@@ -29,7 +29,12 @@ const nextConfig = {
         permanent: true,
       }
     ]
-  }
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false };
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
