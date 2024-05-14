715c884df05ffbd1c8ed949ea812d238d8182cae
moreal <dev.moreal@gmail.com>
2024-05-14 09:12:23 +0900

    fix: check required environment lazy

--- sdk.ts
+++ sdk.ts
@@ -2,10 +2,6 @@ import { GetServerSidePropsContext } from "next/types";
 import { getSdk } from "./generated/graphql-request";
 import { GraphQLClient } from "graphql-request"
 
-if (process.env.NETWORK_CONF_MAP === undefined) {
-    throw new Error("All required environment variables are not set.");
-}
-
 function parseNetworkConfMap(confMapString: string): Map<string, ReturnType<typeof getSdk>> {
     const map = new Map();
     for (const pair of confMapString.split(',')) {
@@ -16,9 +12,14 @@ function parseNetworkConfMap(confMapString: string): Map<string, ReturnType<type
     return map;
 }
 
-const networkToSdkMap = parseNetworkConfMap(process.env.NETWORK_CONF_MAP);
 
 export const networkToSDK = (context: GetServerSidePropsContext) => {
+    if (process.env.NETWORK_CONF_MAP === undefined) {
+        throw new Error("All required environment variables are not set.");
+    }
+
+    const networkToSdkMap = parseNetworkConfMap(process.env.NETWORK_CONF_MAP);
+
     const network = context.query.network;
     if (typeof network !== "string") {
         throw new Error("Network name parameter is not a string.");
