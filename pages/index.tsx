3d16be07f5b18071d3d4119b4ffa3de93d2c9288
devkyoung2 <devkyoung2@gmail.com>
2024-10-25 22:13:02 +0900

    add navigation buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,65 @@
-import type { NextPage } from 'next'
+import Router, { useRouter } from 'next/router';
+import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex justify-center align-middle h-screen">
-      <div className='m-auto'>
-        <b>Nine Chronicles Board</b>
-      </div>
-    </div>
-  )
+interface ButtonProps {
+  children: string;
+  handleClick: () => void;
 }
 
-export default Home
+const Button = ({ children, handleClick }: ButtonProps) => {
+  return (
+    <button
+      type='button'
+      className='border font-semibold text-lg m-1 py-1 w-full bg-white rounded-md  hover:bg-gray-200 transition duration-200'
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+const networks = [
+  {
+    id: 0,
+    name: 'odin',
+  },
+  {
+    id: 1,
+    name: 'odin-internal',
+  },
+  {
+    id: 2,
+    name: 'heimdall',
+  },
+  {
+    id: 3,
+    name: 'heimdall-internal',
+  },
+];
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  const handleClick = (networkName: string) => {
+    router.push(`/${networkName}`);
+  };
+
+  return (
+    <div className='flex justify-center align-items h-screen'>
+      <div className='m-auto'>
+        <h1 className='font-extrabold text-3xl py-5'>Available networks</h1>
+        <ul className='text-center'>
+          {networks.map((network) => (
+            <li key={network.id}>
+              <Button handleClick={() => handleClick(network.name)}>
+                {network.name}
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
